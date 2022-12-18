2e49eaba67df84c2b91bafe049965b4c8a7bfef3
Aleksander <spas.aleksander.v@gmail.com>
2022-12-18 16:31:20 +0300

    Форматирование

--- Семинары/Урок 1. Знакомство с языком программирования С#/3-ее задание/Program.cs
+++ Семинары/Урок 1. Знакомство с языком программирования С#/3-ее задание/Program.cs
@@ -7,7 +7,7 @@ int num = int.Parse(Console.ReadLine() ?? "");
 if (num == 1)
 {
     Console.WriteLine("Понедельник");
-} 
+}
 else if (num == 2)
 {
     Console.WriteLine("Вторник");
@@ -15,19 +15,24 @@ else if (num == 2)
 else if (num == 3)
 {
     Console.WriteLine("Среда");
-}else if (num == 4)
+}
+else if (num == 4)
 {
     Console.WriteLine("Четверг");
-}else if (num == 5)
+}
+else if (num == 5)
 {
     Console.WriteLine("Пятница");
-}else if (num == 6)
+}
+else if (num == 6)
 {
     Console.WriteLine("Суббота");
-}else if (num == 7)
+}
+else if (num == 7)
 {
     Console.WriteLine("Воскресенье");
-}else
-{ 
+}
+else
+{
     Console.WriteLine("Введите корректное число(от 1 до 7)");
 }
