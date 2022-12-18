873147ab125aa57f84b9dc9f83a8dd9ad289a0f5
Aleksander <spas.aleksander.v@gmail.com>
2022-12-18 16:53:07 +0300

    Успешно сделал

--- Семинары/Урок 1. Знакомство с языком программирования С#/4-ое задание/Program.cs
+++ Семинары/Урок 1. Знакомство с языком программирования С#/4-ое задание/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+*/
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine() ?? "");
+
+num = Math.Abs(num);
+
+for (int i = -num; i <= num; i++)
+{
+    Console.Write($"{i}, ");
+}
