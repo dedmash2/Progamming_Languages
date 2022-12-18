334265ad018d21ba30b0e7d72987a9ab7ac3658d
Aleksander <spas.aleksander.v@gmail.com>
2022-12-18 15:34:08 +0300

    Проверка на квадрат

---
+++ Семинары/Урок 1. Знакомство с языком программирования С#/2-ое задание/Program.cs
@@ -0,0 +1,14 @@
+﻿/*
+    Написать программу которая проверяет, является ли первое число квадратом второго
+*/
+string a = Console.ReadLine() ?? "",
+       b = Console.ReadLine() ?? "";
+int numA = int.Parse(a);
+int numB = int.Parse(b);
+if (Math.Pow(numB, 2) == numA)
+{
+    Console.WriteLine("да");
+} else
+{
+    Console.WriteLine("нет");
+}
