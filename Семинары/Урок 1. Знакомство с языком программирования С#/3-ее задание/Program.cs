723779397f3587fe1de74d5826f95b60a225a5e4
Aleksander <spas.aleksander.v@gmail.com>
2022-12-18 16:10:28 +0300

    На номер день недели выскакивает

--- Семинары/Урок 1. Знакомство с языком программирования С#/3-ее задание/Program.cs
+++ Семинары/Урок 1. Знакомство с языком программирования С#/3-ее задание/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+    Напишите программу, которая будет выдавать название дня недели по заданному номеру
+*/
+Console.WriteLine("Введите номер дня недели(от 1 до 7): ");
+int num = int.Parse(Console.ReadLine() ?? "");
+
+if (num == 1)
+{
+    Console.WriteLine("Понедельник");
+} 
+else if (num == 2)
+{
+    Console.WriteLine("Вторник");
+}
+else if (num == 3)
+{
+    Console.WriteLine("Среда");
+}else if (num == 4)
+{
+    Console.WriteLine("Четверг");
+}else if (num == 5)
+{
+    Console.WriteLine("Пятница");
+}else if (num == 6)
+{
+    Console.WriteLine("Суббота");
+}else if (num == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
