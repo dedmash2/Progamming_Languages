ea30f800fdd32872e68d2773b3478ae958d616a8
Aleksander <spas.aleksander.v@gmail.com>
2022-12-18 15:19:17 +0300

    Исправление ошибок

--- Семинары/Урок 1. Знакомство с языком программирования С#/1-ое задание/Program.cs
+++ Семинары/Урок 1. Знакомство с языком программирования С#/1-ое задание/Program.cs
@@ -2,5 +2,5 @@
 Напишите программу, которая на вход принимает число и выдает его квадрат
 */
 Console.Write("Введите число, квадрат которого вы хотите посчитать: ");
-int num = int.Parse(Console.ReadLine() ?? "");
+int num = int.Parse(Console.ReadLine() ?? string.Empty);
 Console.WriteLine($"Квадрат числа {num} равен: {num * num}.");
