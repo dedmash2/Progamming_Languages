e384b6aee586452779cf60997c5cbf797591302a
Aleksander <spas.aleksander.v@gmail.com>
2022-12-18 12:43:22 +0300

    text

--- Лекции/1 Знакомство с языком программирования C#/Example007_Magic/Program.cs
+++ Лекции/1 Знакомство с языком программирования C#/Example007_Magic/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+int xa = 20, ya = 1,
+    xb = 1, yb = 20,
+    xc = 40, yc = 20;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
+
+int x = xa, y = xb;
+
+int count = 100;
